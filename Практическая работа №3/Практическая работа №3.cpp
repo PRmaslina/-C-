3a1dad18fdea55d84d27c49568996ed700c0786c
PajiloyEvrei <121241426+PajiloyEvrei@users.noreply.github.com>
2024-11-12 00:47:49 +0300

    3

--- Практическая работа №3/Практическая работа №3.cpp
+++ Практическая работа №3/Практическая работа №3.cpp
@@ -7,30 +7,24 @@ using namespace std;
 HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
 template<const int sizeMat>
-void printMat(int mat[][sizeMat], int* turtle) {
+void printMat(int mat[][sizeMat]) {
 	cout << endl;
 	for (int i = 0; i < sizeMat; ++i) {
 		for (int j = 0; j < sizeMat; ++j) {
-			SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-			if (turtle == &mat[j][i]) {
-				SetConsoleTextAttribute(handle, FOREGROUND_RED);
-				cout << setw(3) << '@' << ' ';
+			if (mat[j][i] == 0) {
+				cout << setw(3) << ' ' << ' ';
 			}
 			else {
-				if (mat[j][i] != 0) {
-					SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
-				}
 				cout << setw(3) << mat[j][i] << ' ';
 			}
 		}
-		SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		cout << endl;
 	}
 }
 
 int main(){
 	setlocale(0, "");
-	const int sizeMat = 10;
+	const int sizeMat = 15;
 	int mat[sizeMat][sizeMat], c=1;
 	for (int i = 0; i < sizeMat; ++i) {
 		for (int j = 0; j < sizeMat; ++j) {
@@ -39,31 +33,57 @@ int main(){
 		}
 		cout << endl;
 	}
-	char side;
-	int range, k = 0;
+	char side = 'r';
+	int k = 0;
 	char ;
 	int* turtle = &mat[0][0];
 	do {
 		cout << "ввдете сторону(l/r/d/u) и дальность движения черепашки:\n";
-		cin >> side >> range;
+		//cin >> side >> range;
+		//switch (side) {
+		//case 'r':
+		//	k = sizeMat;
+		//	break;
+		//case 'l':
+		//	k = -sizeMat;
+		//	break;
+		//case 'u':
+		//	k = -1;
+		//	break;
+		//case 'd':
+		//	k = 1;
+		//	break;
+		//}
+		for (int i = 0; i < sizeMat; ++i, ++c) {
+			turtle += k;
+			if (*turtle == 0) {
+				*turtle = c;
+			}
+			else {
+				turtle -= k;
+				break;
+			}
+		}
 		switch (side) {
 			case 'r':
 				k = sizeMat;
-				break;
-			case 'l':
-				k = -sizeMat;
-				break;
-			case 'u':
-				k = -1;
+				side = 'd';
 				break;
 			case 'd':
 				k = 1;
+				side = 'l';
+				break;
+			case 'l':
+				k = -sizeMat;
+				side = 'u';
+				break;
+			case 'u':
+				k = -1;
+				side = 'r';
 				break;
 		}
-		for (int i = 0; i < range; ++i, ++c) {
-			*turtle = c;
-			turtle = turtle + k;
-		}
-		printMat<sizeMat>(mat, turtle);
+		system("cls");
+		printMat<sizeMat>(mat);
+		Sleep(10);
 	} while (side != 'n');
 }
